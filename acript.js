3335b786911740f4dd72df46175073a84576c4dc
SalinasJason <jasonsalinas770@gmail.com>
2024-11-06 21:58:16 -0800

    added answer number 2

--- acript.js
+++ acript.js
@@ -1,3 +1,7 @@
 let myGrades = [100, 95, 80, 75, 70];
 console.log(myGrades[0]); // Outputs: 100
 console.log(myGrades[4]); // Outputs: 70
+
+for (let i = 0; i < myGrades.length; i++){
+    console.log(myGrades[i]); // Outputs 100 95 80 75 70
+}
