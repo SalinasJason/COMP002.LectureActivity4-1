c6003c0e77e638480deb08676305c7a9c3d7c229
SalinasJason <jasonsalinas770@gmail.com>
2024-11-06 22:10:50 -0800

    added answer number 3

--- acript.js
+++ acript.js
@@ -5,3 +5,15 @@ console.log(myGrades[4]); // Outputs: 70
 for (let i = 0; i < myGrades.length; i++){
     console.log(myGrades[i]); // Outputs 100 95 80 75 70
 }
+
+let myGrades2 = [100, 95, 80, 75, 70];
+myGrades2.push(60);
+myGrades2.push(50);
+myGrades2.push(40);
+myGrades2.push(30);
+myGrades2.push(20);
+console.log(myGrades2);
+console.log(myGrades2.pop());
+console.log(myGrades2.pop());
+console.log(myGrades2.pop());
+console.log(myGrades2);
